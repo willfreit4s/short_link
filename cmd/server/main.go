e3f74a4bb15041432921e5814470a10859478562
willfreit4s <williansilvarj96@gmail.com>
2026-07-13 16:16:27 -0300

    feat: add graceful restart or stop on server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/willfreit4s/short_link/internal/handler"
 	"github.com/willfreit4s/short_link/internal/usecase"
@@ -18,5 +26,26 @@ func main() {
 		v1.POST("/links", shortLinkHandler.CreateShortLink)
 	}
 
-	router.Run()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
