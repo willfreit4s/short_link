acca7a0d3c66ec5fe052cf65d626d5e5d8574864
willfreit4s <williansilvarj96@gmail.com>
2026-07-24 16:36:49 -0300

    fix: ajust on migrations

--- sql/migrations/000001_init.up.sql
+++ sql/migrations/000001_init.up.sql
@@ -3,7 +3,7 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS links (
     id varchar(12) NOT NULL PRIMARY KEY,
     original_url text NOT NULL,
-    created_at timestamp NOT NULL DEFAULT now(),
-)
+    created_at timestamp NOT NULL DEFAULT now()
+);
 
 COMMIT;
