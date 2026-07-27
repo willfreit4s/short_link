7503c3661f99b6f3eba8628a709734a6e165f53c
willfreit4s <williansilvarj96@gmail.com>
2026-07-27 08:46:39 -0300

    fix: adjust log on use case

--- internal/usecase/short_link.go
+++ internal/usecase/short_link.go
@@ -32,22 +32,22 @@ func NewShortLinkUseCase(repository ports.ShortLinkRepository, transactionManage
 
 func (uc *shortLinkUseCase) CreateShortLink(ctx context.Context, input usecasedto.CreateShortLinkInput) (usecasedto.CreateShortLinkOutput, error) {
 	log := logger.FromContext(ctx)
-	log.Info("creating short link")
 
 	shortLink, err := entity.NewShortLink(input.OriginalURL)
 	if err != nil {
+		log.Error("failed to create short link entity", "error", err)
 		return usecasedto.CreateShortLinkOutput{}, err
 	}
 
 	createdShortLink, err := uc.repository.CreateShortLink(ctx, shortLink)
 	if err != nil {
+		log.Error("failed to create short link in repository", "error", err)
 		return usecasedto.CreateShortLinkOutput{}, err
 	}
 	if createdShortLink == nil {
+		log.Error("short link repository returned nil short link")
 		return usecasedto.CreateShortLinkOutput{}, errors.New("short link repository returned nil short link")
 	}
-
-	log.Info("short link created", "short_link_id", createdShortLink.ID)
 
 	return usecasedto.CreateShortLinkOutput{
 		ID:          createdShortLink.ID.String(),
@@ -58,17 +58,16 @@ func (uc *shortLinkUseCase) CreateShortLink(ctx context.Context, input usecasedt
 
 func (uc *shortLinkUseCase) GetShortLink(ctx context.Context, input usecasedto.GetShortLinkInput) (usecasedto.GetShortLinkOutput, error) {
 	log := logger.FromContext(ctx)
-	log.Info("resolving short link", "short_link_id", input.Hash)
 
 	shortLink, err := uc.repository.GetShortLink(ctx, input.Hash)
 	if err != nil {
+		log.Error("failed to get short link from repository", "error", err)
 		return usecasedto.GetShortLinkOutput{}, err
 	}
 	if shortLink == nil {
+		log.Error("short link not found", "hash", input.Hash)
 		return usecasedto.GetShortLinkOutput{}, ErrShortLinkNotFound
 	}
-
-	log.Info("short link resolved", "short_link_id", shortLink.ID)
 
 	return usecasedto.GetShortLinkOutput{OriginalURL: shortLink.OriginalURL}, nil
 }
