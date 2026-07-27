5e7bb93bed0d12c27f774675e30b6aadf0b4b5ea
willfreit4s <williansilvarj96@gmail.com>
2026-07-27 09:57:27 -0300

    feat: add short link cache interface on ports

--- internal/ports/short_link.go
+++ internal/ports/short_link.go
@@ -10,3 +10,9 @@ type ShortLinkRepository interface {
 	CreateShortLink(ctx context.Context, shortLink *entity.ShortLink) (*entity.ShortLink, error)
 	GetShortLink(ctx context.Context, id string) (*entity.ShortLink, error)
 }
+
+type ShortLinkCache interface {
+	Get(ctx context.Context, hash string) (string, bool, error)
+	Set(ctx context.Context, hash string, originalURL string) error
+	Delete(ctx context.Context, hash string) error
+}
