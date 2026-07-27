46adfcff03966c1fbd4d6d12da13618f32e5b825
willfreit4s <williansilvarj96@gmail.com>
2026-07-27 09:57:10 -0300

    feat: add redis on bootstrap

--- internal/bootstrap/router.go
+++ internal/bootstrap/router.go
@@ -2,19 +2,22 @@ package bootstrap
 
 import (
 	"log/slog"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	redisclient "github.com/redis/go-redis/v9"
 	"github.com/willfreit4s/short_link/internal/handler"
 	repositorypostgres "github.com/willfreit4s/short_link/internal/repository/postgres"
+	repositoryredis "github.com/willfreit4s/short_link/internal/repository/redis"
 	"github.com/willfreit4s/short_link/internal/usecase"
 	"github.com/willfreit4s/short_link/pkg/logger"
 )
 
-func NewRouter(log *slog.Logger, conn *pgxpool.Pool) *gin.Engine {
+func NewRouter(log *slog.Logger, conn *pgxpool.Pool, redisConn *redisclient.Client, redisTTL time.Duration) *gin.Engine {
 	shortLinkRepository := repositorypostgres.NewShortLinkRepository(conn)
-	transactionManager := repositorypostgres.NewTransactionManager(conn)
-	shortLinkUseCase := usecase.NewShortLinkUseCase(shortLinkRepository, transactionManager)
+	shortLinkCache := repositoryredis.NewShortLinkCache(redisConn, redisTTL)
+	shortLinkUseCase := usecase.NewShortLinkUseCase(shortLinkRepository, shortLinkCache)
 	shortLinkHandler := handler.NewShortLinkHandler(shortLinkUseCase)
 
 	router := gin.New()
